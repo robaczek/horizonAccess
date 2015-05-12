657281f320928089573769c2e9590ce5ac0d8ccd
Worm <longworm@outlook.com>
2015-05-12 21:59:25 +0200

    Initial commit. Working version.

---
+++ horizonAccess.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""This simple module allows to retrieve information about books
+checked out in the library using Dynix Horizon software. Only basic
+functionality is provided.
+
+Usage example:
+import horizonAccess
+l = horizonAccess.Library('LIBRARY_URL_ENDING_IN.jsp', 'ID', 'PASSWORD')
+l.mybooks()
+
+Author: Wiktor Gołgowski <wgolgowski@gmail.com>
+License: WTFPL <http://www.wtfpl.net>
+"""
+
+from __future__ import print_function
+
+import urllib as U
+import xml.etree.ElementTree as ET
+
+
+class Library:
+    def __init__(self, url, idnum, code, debug=False):
+
+        """
+        Keyword Arguments:
+        url   -- base URL of the library,
+        idnum -- ID number,
+        code  -- access code.
+        """
+        self.debug = debug
+        self.baseUrl = url
+        self.idnum = idnum
+        self.code = code
+        self.authorized = False
+        self.session = ''
+
+    def auth(self):
+        """
+        Creates new session with provided credentials.
+        """
+        # Get session ID:
+        resp = U.request.urlopen(self.baseUrl + '?auth=true&GetXML=true')
+        tree = ET.fromstring(resp.read())
+        sessid = tree.find('session').text
+        if self.debug:
+            print('Session ID: %s' % sessid)
+
+        # Authorize:
+        params = {
+            'session': sessid,
+            'sec1': self.code,
+            'sec2': self.idnum,
+        }
+
+        data = U.parse.urlencode(params)
+        req = U.request.Request(self.baseUrl + '?GetXML=true',
+                                data.encode('ascii'))
+        resp = U.request.urlopen(req)
+        xx = resp.read()
+        tree = ET.fromstring(xx)
+        auth = tree.find('security/auth').text
+        if auth == 'true':
+            if self.debug:
+                print('Authorized.')
+            self.session = sessid
+            self.authorized = True
+            return True
+        else:
+            if self.debug:
+                print('Authorization failed.')
+            self.authorized = False
+            return False
+
+    def mybooks(self):
+        """
+        Returns a dictionary consisting of error code (0 if success) and a
+        list of borrowed books. Every book is a tuple with name, due
+        date, checkout date and number of prolongates.
+
+        """
+        retval = {'status': 0, 'books': []}
+        if not self.authorized and not self.auth():
+            if self.debug:
+                print('Cannot authorize. Check your URL/credentials.')
+            retval['status'] = 1
+            return retval
+        else:
+            params = {
+                'session': self.session,
+                'menu': 'account',
+                'submenu': 'itemsout'
+            }
+            req = U.request.Request(self.baseUrl + '?GetXML=true',
+                                    U.parse.urlencode(params).encode('ascii'))
+            resp = U.request.urlopen(req)
+            tree = ET.fromstring(resp.read())
+            auth = tree.find('security/auth').text
+            if auth != 'true':
+                if self.debug:
+                    print('Request failed: not authenticated (timeout?).')
+                retval['status'] = 2
+                return retval
+            else:
+                books = tree.find('itemsoutdata')
+                blist = []
+                for book in books.findall('itemout'):
+                    blist.append((book.find('disptitle').text,
+                                  book.find('duedate').text,
+                                  book.find('ckodate').text,
+                                  book.find('numrenewals').text))
+                retval['books'] = blist
+                return retval
